536294bc228564638b6ff02a26f91995e7454c45
Kaivalya <kaivalyamanit@gmail.com>
2022-06-18 13:31:41 +0530

    Fourth Commit

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/transfer' ,(req,res) => {
             });
         }
     })
-    res.redirect('https://kaivalya4.github.io/Banking-Solutions-frontend/FrontEnd/Pages/Customer.html');
+    res.redirect('https://kaivalya4.github.io/Banking-Solutions-frontend/Pages/Customer.html');
 } )
 
 app.get('/details/:acc' , (req,res) => {
