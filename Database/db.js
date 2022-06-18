a30584e57665e0df2414ac2f8bc95f0a70221b21
Kaivalya <kaivalyamanit@gmail.com>
2022-06-18 13:11:33 +0530

    Second commit

--- Database/db.js
+++ Database/db.js
@@ -1,12 +1,20 @@
 const {Client} = require('pg');
-
+/*
 const client  = new Client({
     host : "localhost",
     user : "postgres" , 
     port : 5432,
     password : "kt6819" ,
     database : "demo1"
-})
+})*/
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
+
 client.connect();
 
 module.exports = client;
